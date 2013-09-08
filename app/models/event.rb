4924cb50ad44cecba612975499159a1512b55fc4
mreich <reich.michael@gmail.com>
2013-09-08 16:28:53 -0400

    Added price to Events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,13 @@
 class Event < ActiveRecord::Base
-  attr_accessible :title, :description, :website, :image
+  attr_accessible :title, :description, :website, :image, :price
 
   validates :user_id, :title, :description, presence: true
   has_attached_file :image, styles: { medium: "320x240>"}
   validates_attachment :image, 
                             content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
                             size: { less_than: 5.megabytes }
+
+  validates :price, :format => { :with => /^\d+??(?:\.\d{0,2})?$/ }, :numericality => {:greater_than => 0, :less_than => 1000}
 
   belongs_to :user
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130908142729) do
+ActiveRecord::Schema.define(:version => 20130908201432) do
 
   create_table "events", :force => true do |t|
     t.string   "description"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(:version => 20130908142729) do
     t.datetime "image_updated_at"
     t.string   "title"
     t.string   "website"
+    t.decimal  "price"
   end
 
   add_index "events", ["user_id"], :name => "index_events_on_user_id"
