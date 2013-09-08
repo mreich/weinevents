2fe61effd39738436376472777a1d40ec652affa
mreich <reich.michael@gmail.com>
2013-09-08 09:31:42 -0400

    Add Pagination

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,7 +5,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.order("created_at desc")
+    @events = Event.order("created_at desc").page(params[:page]).per_page(20)
 
     respond_to do |format|
       format.html # index.html.erb
