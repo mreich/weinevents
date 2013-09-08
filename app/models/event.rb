4d6030ebb2cefca2d0a17303e2fad657b36c442a
mreich <reich.michael@gmail.com>
2013-09-08 10:34:25 -0400

    Add Website to Events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  attr_accessible :title, :description, :image
+  attr_accessible :title, :description, :website, :image
 
   validates :user_id, :title, :description, presence: true
   has_attached_file :image, styles: { medium: "320x240>"}
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130908140720) do
+ActiveRecord::Schema.define(:version => 20130908142729) do
 
   create_table "events", :force => true do |t|
     t.string   "description"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20130908140720) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.string   "title"
+    t.string   "website"
   end
 
   add_index "events", ["user_id"], :name => "index_events_on_user_id"
