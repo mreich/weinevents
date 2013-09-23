9568ac6949c505e8667f0040a0498eeaf5f5ae97
mreich <reich.michael@gmail.com>
2013-09-23 19:33:29 -0400

    Remove Location Text Field From Event Form

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,5 @@
 class Location < ActiveRecord::Base
-  attr_accessible :city, :description, :email, :name, :phone, :postalcode, :sitecity_id, :street1, :street2, :user_id, :website, :state_id, :country_id
+  attr_accessible :city, :description, :email, :name, :phone, :postalcode, :sitecity_id, :street1, :street2, :user_id, :website, :state_id, :country_id, :url
 
 	#validates :name, :street1, :postalcode, :city, :user_id, :sitecity_id, :state_id, :country_id, presence: true
 
