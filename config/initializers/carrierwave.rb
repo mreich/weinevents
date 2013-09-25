834dfdecd25a7b27d9caea49748abe6797f2c58f
mreich <reich.michael@gmail.com>
2013-09-25 17:53:57 -0400

    Bug fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,8 +4,8 @@ CarrierWave.configure do |config|
   config.permissions = 0666
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
-    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
+    :aws_access_key_id      => 'AKIAIDOQDUMD33UEK2XA',
+    :aws_secret_access_key  => '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',
   }
   config.fog_directory  = 'weinevents'
 end
