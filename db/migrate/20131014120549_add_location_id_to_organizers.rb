cdfa6e6c40f5b93bff75c2f989dce3e883fb8d1d
mreich <reich.michael@gmail.com>
2013-10-14 17:39:23 -0400

    Fix migration and bug

--- db/migrate/20131014120549_add_location_id_to_organizers.rb
+++ db/migrate/20131014120549_add_location_id_to_organizers.rb
@@ -1,4 +1,5 @@
 class AddLocationIdToOrganizers < ActiveRecord::Migration
   def change
+		add_column "organizers", "location_id", :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131014113236) do
+ActiveRecord::Schema.define(:version => 20131014120549) do
 
   create_table "countries", :force => true do |t|
     t.string   "name"
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(:version => 20131014113236) do
     t.string   "url"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "location_id"
   end
 
   add_index "organizers", ["url"], :name => "index_organizers_on_url", :unique => true
